14f0034e4e3720e52a30b58d27d9bb6f9c248a75
Somesh <kolisomesh27@gmail.com>
2020-06-06 04:26:59 +0530

    Adds basic boilerplate setup for new sdkgenerators  - Adds basic files to boiler plate    - gitignore    - npmignore    - index.js    - README.md    - indes.js    - lib folder with index and util files    - package file  - Adds script to add new new sdk gen to /sdkgen folder  - Adds script to package file (boilerplate)

---
+++ npm/boilerplate.js
@@ -0,0 +1,30 @@
+var exec = require('shelljs').exec,
+    fs = require('fs'),
+    chalk = require('chalk'),
+    codegens,
+    path = require('path'),
+    getSubfolders = (folder) => {
+        return fs.readdirSync(folder)
+            .map((subfolder) => { return subfolder; });
+    };
+const args = process.argv,
+    BOILERPLATE = path.resolve(__dirname, '../npm/boilerplate'),
+    SDKGEN_FOLDER = path.resolve(__dirname, '../sdkgen') + '/';
+
+codegens = getSubfolders(SDKGEN_FOLDER);
+
+if (!args[2]) {
+    console.log(chalk.red('Please provide a name for the codegen.'));
+    return;
+}
+
+if (codegens.includes(args[2])) {
+    console.log(chalk.red('SDKGEN with same name already exists. ' +
+        'Please follow the naming convention(language-library(variant)) and choose a unique name.\n'));
+    return;
+}
+
+exec('cp -a ' + BOILERPLATE + '/. ' + SDKGEN_FOLDER + args[2] + '/');
+
+console.log(chalk.yellow('A folder named ' + args[2] +
+  ' has been added to /sdkgen. This folder contains the basic structure of a sdk-generator.\n'));
