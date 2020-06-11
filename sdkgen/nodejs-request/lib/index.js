056122d82db2b7cd9007aedb321142f10c2484d1
Somesh <kolisomesh27@gmail.com>
2020-06-11 23:52:17 +0530

    Fixes typo in nodejs-request snippet

--- sdkgen/nodejs-request/lib/index.js
+++ sdkgen/nodejs-request/lib/index.js
@@ -1,59 +1,66 @@
-//TODO add options and fetch options
+// TODO add options and fetch options
 const sdk = require('postman-collection'),
-    { getLanguageList} = require('postman-code-generators'),
+    {
+        getLanguageList
+    } = require('postman-code-generators'),
     getCodegenOptions = require('postman-code-generators').getOptions,
     processCollection = require('./util').processCollection;
-    _ = require('lodash');
+_ = require('lodash');
 
 /**
  * Generates sdk for nodejs-request
+
  * @param {PostmanCollection} collection - Postman collection Instance
  * @param {Object} options - postman-code-generators options
+ * @param {Function} callback - callback functio to return results (err, response)
  * @returns {String} - sdk snippet for input collection
  * TODO add indentation to entire snippet
  */
-async function generate(collection, options, callback){
+async function generate (collection, options, callback) {
     var snippet = '',
-        collectionVariables = collection.variables ? collection.variables : [],
-        sdkname = collection.name.split(' ').join('_') ;
-    if(options.ES6_enabled){
+        collectionVariables = collection.variables.members.length ? collection.variables.members : [],
+        // sdkname = collection.name.split(' ').join('_'),
+        sdkname = 'SDKNAME',
+        indent = options.indentType === 'Tab' ? '\t' : ' ';
+    indent = indent.repeat(options.indentCount);
+    if (options.ES6_enabled) {
         snippet += 'const ';
     }
-    else{
+    else {
         snippet += 'var ';
     }
-    snippet += 'request = require(\'nodejs-request\');\n';
-    snippet += `function ${sdkname}() {\n`;
-    snippet += 'this.requests = {\n';
-    try{
-        snippet += await processCollection(collection, options,1);
+    snippet += 'request = require(\'request\');\n';
+    snippet += `function ${sdkname}(environment) {\n`;
+    snippet += indent + 'this.requests = {\n';
+    try {
+        snippet += await processCollection(collection, options, 1);
     }
-    catch(error){
+    catch (error) {
         return callback(error, null);
     }
-    snippet += '};\n'
-    snippet += 'this.variables = {\n';
-    // TODO extract and add variables here
-    // await collectionVariables.forEach(item =>{
-    //     snippet += `${item.key}: '${item.value}',\n`
-    // })
-    // snippet += '}\n';
-    snippet += `${sdkname}.prototype.setVariabes = function(variables) {\n`
-    snippet += 'Object.keys(variables).forEach((key) => {\n';
-    snippet += 'this.variables[key] = variables.key;\n';
-    snippet += '});\n'
-    snippet += '}\n'
+    snippet += indent + '};\n';
+    snippet += indent + 'this.variables = {\n';
+    await collectionVariables.forEach((item) => {
+        snippet += indent.repeat(2) + `${item.key}: '${item.value}',\n`;
+    });
+    snippet += indent + '}\n';
+    snippet += indent + `${sdkname}.prototype.setVariabes = function(variables) {\n`;
+    snippet += indent.repeat(2) + 'Object.keys(variables).forEach((key) => {\n';
+    snippet += indent.repeat(3) + 'this.variables[key] = variables.key;\n';
+    snippet += indent.repeat(2) + '});\n';
+    snippet += indent + '}\n';
+    snippet += '}\n';
     return callback(null, snippet);
 }
 
 /**
  * Gives a list of possible options for nodejs-request sdk generator
+ * TODO update and test this function
  */
-function getOptions(){
+function getOptions () {
     var result;
     getCodegenOptions('NodeJs', 'Request', (err, codegenOptions) => {
-        if(err)
-        {
+        if (err) {
             result = err;
             return;
         }
@@ -65,4 +72,4 @@ function getOptions(){
 module.exports = {
     generate,
     getOptions
-}
+};
