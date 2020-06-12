d05f87c23c6c11373d3daf199406c488d0836bf9
Somesh <kolisomesh27@gmail.com>
2020-06-12 06:42:28 +0530

    Adds checkoption method to sdkgenerator to check options  - Fixes broken tests  - Modifies errors

--- test/unit/sdkgen.test.js
+++ test/unit/sdkgen.test.js
@@ -1,59 +1,57 @@
+/* eslint-disable max-len */
 const expect = require('chai').expect,
     sdkgen = require('../../'),
     postmanEchoCollection = require('../fixtures/Postman Echo.postman_collection.json'),
-    sdk = require('postman-collection');
-
+    sdk = require('postman-collection'),
+    sdkOptions = {
+        language: 'Nodejs',
+        Variant: 'Request'
+    };
 describe('Generate functon', () => {
-    it('should generate sdk snippet without sdkgenoptions', () => {
-        sdkgen.generate(postmanEchoCollection, {}, {}, (err, snippet) => {
-            expect(err).to.be.null;
-            expect(snippet).to.include('request = require(\'nodejs-request\')');
-        });
-    });
 
     it('should generate sdk snippet with output type option', () => {
         sdkgen.generate(postmanEchoCollection, {
             outputType: 'String'
-        }, {}, (err, snippet) => {
+        }, sdkOptions, (err, snippet) => {
             expect(err).to.be.null;
-            expect(snippet).to.include('request = require(\'nodejs-request\')');
+            expect(snippet).to.include('request = require(\'request\')');
         });
     });
 
     it('should generate sdk snippet with source as collection json', () => {
-        sdkgen.generate(postmanEchoCollection, {}, {}, (err, snippet) => {
+        sdkgen.generate(postmanEchoCollection, sdkOptions, {}, (err, snippet) => {
             expect(err).to.be.null;
-            expect(snippet).to.include('request = require(\'nodejs-request\')');
+            expect(snippet).to.include('request = require(\'request\')');
         });
     });
 
     it('should generate sdk snippet with source as PostmanCollection instance', () => {
         var collection = new sdk.Collection(postmanEchoCollection);
-        sdkgen.generate(collection, {}, {}, (err, snippet) => {
+        sdkgen.generate(collection, sdkOptions, {}, (err, snippet) => {
             expect(err).to.be.null;
-            expect(snippet).to.include('request = require(\'nodejs-request\')');
+            expect(snippet).to.include('request = require(\'request\')');
         });
     });
 
     it('should generate sdk snippet with source as url', () => {
-        sdkgen.generate('https://www.getpostman.com/collections/4a7c7e33aa5dc9dd11b1', {}, {}, (err, snippet) => {
-            expect(err).to.be.null;
-            expect(snippet).to.include('request = require(\'nodejs-request\')');
-        });
-    });
-
-    it('should generate sdk snippet with source as file path', () => {
-        sdkgen.generate('/home/wolf/development/codegen/postman-collection-code-generators/test/fixtures/Postman Echo.postman_collection.json',
-            {}, {}, (err, snippet) => {
+        sdkgen.generate('https://www.getpostman.com/collections/4a7c7e33aa5dc9dd11b1',
+            sdkOptions, {}, (err, snippet) => {
                 expect(err).to.be.null;
-                expect(snippet).to.include('request = require(\'nodejs-request\')');
+                expect(snippet).to.include('request = require(\'request\')');
             });
     });
 
+    it('should generate sdk snippet with source as file path', () => {
+        sdkgen.generate('/home/wolf/development/codegen/postman-collection-code-generators/test/fixtures/Postman Echo.postman_collection.json', {}, {}, (err, snippet) => {
+            expect(err).to.be.null;
+            expect(snippet).to.include('request = require(\'request\')');
+        });
+    });
+
     it('should throw an error if invalid colleciton source is provided', () => {
-        sdkgen.generate('random letters', {}, {}, (err, snippet) => {
+        sdkgen.generate('random letters', sdkOptions, {}, (err, snippet) => {
             expect(snippet).to.be.null;
-            expect(err).not.to.be.null;
+            expect(err);
         });
     });
     // TODO update this test for better approach
@@ -66,7 +64,7 @@ describe('Generate functon', () => {
             requestTimeout: 0,
             SDKGEN_enabled: true
         };
-        sdkgen.generate(postmanEchoCollection, {}, options, (err, snippet) => {
+        sdkgen.generate(postmanEchoCollection, sdkOptions, options, (err, snippet) => {
             expect(err).to.be.null;
             expect(snippet).to.include('\t\t');
         });
