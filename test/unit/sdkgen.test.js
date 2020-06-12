0bac247dad8baf0156b9f7f180ce4b91ed77df6a
Somesh <kolisomesh27@gmail.com>
2020-06-13 03:07:46 +0530

    Adds option to output to a file  - Adds check to check if outputtype is file  - Adds check for file path  - Outputs file to given location  - Adds test to test output file

--- test/unit/sdkgen.test.js
+++ test/unit/sdkgen.test.js
@@ -2,24 +2,60 @@
 const expect = require('chai').expect,
     sdkgen = require('../../'),
     postmanEchoCollection = require('../fixtures/Postman Echo.postman_collection.json'),
-    sdk = require('postman-collection'),
-    sdkOptions = {
-        language: 'Nodejs',
-        Variant: 'Request'
-    };
-describe('Generate functon', () => {
+    sdk = require('postman-collection');
+describe('Generate function', () => {
 
-    it('should generate sdk snippet with output type option', () => {
+    it('should generate sdk snippet without outputType option', () => {
         sdkgen.generate(postmanEchoCollection, {
-            outputType: 'String'
-        }, sdkOptions, (err, snippet) => {
+            language: 'Nodejs',
+            variant: 'request'
+        }, {}, (err, snippet) => {
             expect(err).to.be.null;
             expect(snippet).to.include('request = require(\'request\')');
         });
     });
 
+    it('should generate sdk snippet with outputType as String', () => {
+        sdkgen.generate(postmanEchoCollection, {
+            language: 'Nodejs',
+            variant: 'request',
+            outputType: 'String'
+        }, {}, (err, snippet) => {
+            expect(err).to.be.null;
+            expect(snippet).to.include('request = require(\'request\')');
+        });
+    });
+
+    it('should generate sdk snippet with outputType as File', () => {
+        sdkgen.generate(postmanEchoCollection, {
+            language: 'Nodejs',
+            variant: 'request',
+            outputType: 'File',
+            // ../sdk.js resolves to root folder because this file is being called from npm folder
+            outputFilePath: '../sdk.js'
+        }, {}, (err, output) => {
+            expect(err).to.be.null;
+            expect(output).to.include('File exported at location:');
+        });
+    });
+
+    it('should throw an error if outputfilepath is invalid', () => {
+        sdkgen.generate(postmanEchoCollection, {
+            language: 'Nodejs',
+            variant: 'request',
+            outputType: 'File',
+            outputFilePath: '../'
+        }, {}, (err, output) => {
+            expect(err).not.to.be.null;
+            expect(output).to.be.null;
+        });
+    });
+
     it('should generate sdk snippet with source as collection json', () => {
-        sdkgen.generate(postmanEchoCollection, sdkOptions, {}, (err, snippet) => {
+        sdkgen.generate(postmanEchoCollection, {
+            language: 'Nodejs',
+            variant: 'request'
+        }, {}, (err, snippet) => {
             expect(err).to.be.null;
             expect(snippet).to.include('request = require(\'request\')');
         });
@@ -27,33 +63,46 @@ describe('Generate functon', () => {
 
     it('should generate sdk snippet with source as PostmanCollection instance', () => {
         var collection = new sdk.Collection(postmanEchoCollection);
-        sdkgen.generate(collection, sdkOptions, {}, (err, snippet) => {
+        sdkgen.generate(collection, {
+            language: 'Nodejs',
+            variant: 'request'
+        }, {}, (err, snippet) => {
             expect(err).to.be.null;
             expect(snippet).to.include('request = require(\'request\')');
         });
     });
 
     it('should generate sdk snippet with source as url', () => {
-        sdkgen.generate('https://www.getpostman.com/collections/4a7c7e33aa5dc9dd11b1',
-            sdkOptions, {}, (err, snippet) => {
-                expect(err).to.be.null;
-                expect(snippet).to.include('request = require(\'request\')');
-            });
+        sdkgen.generate('https://www.getpostman.com/collections/4a7c7e33aa5dc9dd11b1', {
+            language: 'Nodejs',
+            variant: 'request'
+        }, {}, (err, snippet) => {
+            expect(err).to.be.null;
+            expect(snippet).to.include('request = require(\'request\')');
+        });
     });
 
     it('should generate sdk snippet with source as file path', () => {
-        sdkgen.generate('/home/wolf/development/codegen/postman-collection-code-generators/test/fixtures/Postman Echo.postman_collection.json', {}, {}, (err, snippet) => {
+        // here the path is `../test/fix....` because the calling file is in npm/
+        sdkgen.generate('../test/fixtures/Postman Echo.postman_collection.json', {
+            language: 'Nodejs',
+            variant: 'request'
+        }, {}, (err, snippet) => {
             expect(err).to.be.null;
             expect(snippet).to.include('request = require(\'request\')');
         });
     });
 
     it('should throw an error if invalid colleciton source is provided', () => {
-        sdkgen.generate('random letters', sdkOptions, {}, (err, snippet) => {
+        sdkgen.generate('random letters', {
+            language: 'Nodejs',
+            variant: 'request'
+        }, {}, (err, snippet) => {
             expect(snippet).to.be.null;
             expect(err);
         });
     });
+
     // TODO update this test for better approach
     it('should generate sdk according to codegenoptions provided', () => {
         var options = {
@@ -64,7 +113,10 @@ describe('Generate functon', () => {
             requestTimeout: 0,
             SDKGEN_enabled: true
         };
-        sdkgen.generate(postmanEchoCollection, sdkOptions, options, (err, snippet) => {
+        sdkgen.generate(postmanEchoCollection, {
+            language: 'Nodejs',
+            variant: 'request'
+        }, options, (err, snippet) => {
             expect(err).to.be.null;
             expect(snippet).to.include('\t\t');
         });
