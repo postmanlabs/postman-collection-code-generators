ff46ff9c0e2fba544fffed0a744ce1033c01893e
Somesh <kolisomesh27@gmail.com>
2020-06-16 21:13:10 +0530

    Updates output files

--- output/test.js
+++ output/test.js
@@ -2,14 +2,15 @@
 const SDKNAME = require('./sdk');
 
 var sdk1 = new SDKNAME({
-    eee: 'eee',
-    fff: 'fff',
-    aaa: 'testing',
-    variable_key: 'somesh'
+  eee: 'eee',
+  fff: 'fff',
+  aaa: 'testing',
+  variable_key: 'somesh'
 });
-sdk1.requests['Postman Echo Copy']['Request Methods']['GET Request']((err, res) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(res.body);
-});
+console.log(sdk1);
+sdk1.requests["Postman Echo"]["Request Methods"]["GET Request"]((err, res) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(res.body);
+})
