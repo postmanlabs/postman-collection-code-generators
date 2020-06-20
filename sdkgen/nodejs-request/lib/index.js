64986bf26e23183ecc6718d352a693b5d1945d75
Somesh <kolisomesh27@gmail.com>
2020-06-21 03:01:08 +0530

    Adds es6 checks to snippet and fixes issue with async function calls

--- sdkgen/nodejs-request/lib/index.js
+++ sdkgen/nodejs-request/lib/index.js
@@ -18,40 +18,47 @@ function generate (collection, options, callback) {
     sdkname = 'SDKNAME',
     indent = options.indentType === 'Tab' ? '\t' : ' ';
   indent = indent.repeat(options.indentCount);
-  if (options.ES6_enabled) {
-    snippet += 'const ';
-  }
-  else {
-    snippet += 'var ';
-  }
-  snippet += 'request = require(\'request\');\n';
-  snippet += `function ${sdkname}(environment = null) {\n\n`;
-  snippet += options.ES6_enabled ? 'const ' : 'var ';
-  snippet += 'self = this;\n\n';
-  snippet += indent + 'this.requests = {\n';
   processCollection(collection, options, (err, collectionSnippet) => {
     if (err) {
       return callback(err, null);
     }
-    snippet += collectionSnippet;
-  });
-  snippet += indent + '};\n\n';
-  snippet += indent + 'this.variables = {\n';
-  collectionVariables.forEach((item) => {
-    snippet += indent.repeat(2) + `${item.key}: '${item.value}',\n`;
-  });
-  snippet += indent + '}\n\n';
-  snippet += indent + `${sdkname}.prototype.setVariables = function(variables) {\n`;
-  snippet += indent.repeat(2) + 'Object.keys(variables).forEach((key) => {\n';
-  snippet += indent.repeat(3) + 'this.variables[key] = variables[key];\n';
-  snippet += indent.repeat(2) + '});\n';
-  snippet += indent.repeat(2) + 'return this.variables;\n';
-  snippet += indent + '}\n\n';
 
-  snippet += indent + 'this.variables = environment ? this.setVariables(environment) : this.variables;\n';
-  snippet += '}\n\n';
-  snippet += 'module.exports = SDKNAME;\n';
-  return callback(null, snippet);
+    if (options.ES6_enabled) {
+      snippet += 'const ';
+    }
+    else {
+      snippet += 'var ';
+    }
+    snippet += 'request = require(\'request\');\n';
+    snippet += `function ${sdkname}(environment = null) {\n\n`;
+    snippet += options.ES6_enabled ? 'const ' : 'var ';
+    snippet += 'self = this;\n\n';
+    snippet += indent + 'this.requests = {\n';
+    snippet += collectionSnippet;
+    snippet += indent + '};\n\n';
+    snippet += indent + 'this.variables = {\n';
+    collectionVariables.forEach((item) => {
+      snippet += indent.repeat(2) + `${item.key}: '${item.value}',\n`;
+    });
+    snippet += indent + '}\n\n';
+    if (options.ES6_enabled) {
+      snippet += indent + `${sdkname}.prototype.setVariables = (variables) => {\n`;
+      snippet += indent.repeat(2) + 'Object.keys(variables).forEach((key) => {\n';
+    }
+    else {
+      snippet += indent + `${sdkname}.prototype.setVariables = function (variables) {\n`;
+      snippet += indent.repeat(2) + 'Object.keys(variables).forEach(function (key) {\n';
+    }
+    snippet += indent.repeat(3) + 'this.variables[key] = variables[key];\n';
+    snippet += indent.repeat(2) + '});\n';
+    snippet += indent.repeat(2) + 'return this.variables;\n';
+    snippet += indent + '}\n\n';
+
+    snippet += indent + 'this.variables = environment ? this.setVariables(environment) : this.variables;\n';
+    snippet += '}\n\n';
+    snippet += 'module.exports = SDKNAME;\n';
+    return callback(null, snippet);
+  });
 }
 
 /**
