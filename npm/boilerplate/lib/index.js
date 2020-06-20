c4275adb4f7ec26017539a03a1429ab17e423fe9
Somesh <kolisomesh27@gmail.com>
2020-06-20 18:32:00 +0530

    Adds/Updates util functions to boilerplate  - Modifies process collection for callback based approach  - updates jsdocs for generate function and adds callback param

---
+++ npm/boilerplate/lib/index.js
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+=======
+/**
+ * Generates sdk for nodejs-request
+ *
+ * @param {PostmanCollection} collection - Postman collection Instance
+ * @param {Object} options - postman-code-generators options
+ * @param {Function} callback - Callback function to return result (err, snippet)
+ */
+function generate (collection, options, callback) {
+  // Add generate function here. Refer existing codegen for usage guide
+}
+
+/**
+ * Gives a list of possible options for nodejs-request sdk generator
+ * TODO update and test this
+ */
+function getOptions () {
+  // Add options here
+}
+
+module.exports = {
+  generate,
+  getOptions
+};
+>>>>>>> ad51f10... Adds/Updates util functions to boilerplate
