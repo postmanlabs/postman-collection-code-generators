6c5a8f0557834dd6094f5e6048dc936fa518a669
Somesh <kolisomesh27@gmail.com>
2020-07-05 03:51:45 +0530

    Completes API key authorizer  - adds all test cases {key,val,query,header}  - adds auth more methods to authRequests

--- test/fixtures/auth_requests.js
+++ test/fixtures/auth_requests.js
@@ -33,6 +33,166 @@ module.exports = {
           'key': 'value'
         }]
       }
+    }),
+    'KEY_HEADER': new sdk.Request({
+      'url': {
+        'protocol': 'https',
+        'path': ['get'],
+        'host': ['postman-echo', 'com'],
+        'query': [{
+          'key': 'foo1',
+          'value': 'bar1'
+        }, {
+          'key': 'foo2',
+          'value': 'aa'
+        }],
+        'variable': []
+      },
+      'method': 'GET',
+      'auth': {
+        'type': 'apikey',
+        'apikey': [{
+          'type': 'any',
+          'value': 'header',
+          'key': 'in'
+        }, {
+          'type': 'any',
+          'value': 'key',
+          'key': 'key'
+        }, {
+          'type': 'any',
+          'value': '',
+          'key': 'value'
+        }]
+      }
+    }),
+    'HEADER': new sdk.Request({
+      'url': {
+        'protocol': 'https',
+        'path': ['get'],
+        'host': ['postman-echo', 'com'],
+        'query': [{
+          'key': 'foo1',
+          'value': 'bar1'
+        }, {
+          'key': 'foo2',
+          'value': 'aa'
+        }],
+        'variable': []
+      },
+      'method': 'GET',
+      'auth': {
+        'type': 'apikey',
+        'apikey': [{
+          'type': 'any',
+          'value': 'query',
+          'key': 'in'
+        }, {
+          'type': 'any',
+          'value': '',
+          'key': 'key'
+        }, {
+          'type': 'any',
+          'value': 'val',
+          'key': 'value'
+        }]
+      }
+    }),
+    'KEY_VAL_QUERY': new sdk.Request({
+      'url': {
+        'protocol': 'https',
+        'path': ['get'],
+        'host': ['postman-echo', 'com'],
+        'query': [{
+          'key': 'foo1',
+          'value': 'bar1'
+        }, {
+          'key': 'foo2',
+          'value': 'aa'
+        }],
+        'variable': []
+      },
+      'method': 'GET',
+      'auth': {
+        'type': 'apikey',
+        'apikey': [{
+          'type': 'any',
+          'value': 'query',
+          'key': 'in'
+        }, {
+          'type': 'any',
+          'value': 'key',
+          'key': 'key'
+        }, {
+          'type': 'any',
+          'value': 'value',
+          'key': 'value'
+        }]
+      }
+    }),
+    'KEY_QUERY': new sdk.Request({
+      'url': {
+        'protocol': 'https',
+        'path': ['get'],
+        'host': ['postman-echo', 'com'],
+        'query': [{
+          'key': 'foo1',
+          'value': 'bar1'
+        }, {
+          'key': 'foo2',
+          'value': 'aa'
+        }],
+        'variable': []
+      },
+      'method': 'GET',
+      'auth': {
+        'type': 'apikey',
+        'apikey': [{
+          'type': 'any',
+          'value': 'query',
+          'key': 'in'
+        }, {
+          'type': 'any',
+          'value': 'key',
+          'key': 'key'
+        }, {
+          'type': 'any',
+          'value': '',
+          'key': 'value'
+        }]
+      }
+    }),
+    'QUERY': new sdk.Request({
+      'url': {
+        'protocol': 'https',
+        'path': ['get'],
+        'host': ['postman-echo', 'com'],
+        'query': [{
+          'key': 'foo1',
+          'value': 'bar1'
+        }, {
+          'key': 'foo2',
+          'value': 'aa'
+        }],
+        'variable': []
+      },
+      'method': 'GET',
+      'auth': {
+        'type': 'apikey',
+        'apikey': [{
+          'type': 'any',
+          'value': 'query',
+          'key': 'in'
+        }, {
+          'type': 'any',
+          'value': '',
+          'key': 'key'
+        }, {
+          'type': 'any',
+          'value': '',
+          'key': 'value'
+        }]
+      }
     })
   }
 };
