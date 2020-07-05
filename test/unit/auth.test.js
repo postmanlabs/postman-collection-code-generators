f6b98a442114bf9ab4dba22829d7d7caece8e1c1
Somesh <kolisomesh27@gmail.com>
2020-07-05 06:17:47 +0530

    Adds test for basic and bearer token auth  - adds test for bearer token auth    - when bearer token is provided    - when beaere token is not provided  - adds test for basic auth    - when username pass are given    - when username is given    - when pass is given    - when nothing is given  - adds requests for auth for all abouve cases

--- test/unit/auth.test.js
+++ test/unit/auth.test.js
@@ -7,7 +7,7 @@ describe('Postman Request Authorization', function () {
 
   describe('API KEY authentication', function () {
     it('should add api header to request when in type is header', function () {
-      var request = authRequests.API_KEY.KEY_VAL_HEADER;
+      let request = authRequests.API_KEY.KEY_VAL_HEADER;
       // TODO pass this request to authorize function
       request = authorize(request);
       expect(request).to.be.of.instanceOf(sdk.Request);
@@ -16,7 +16,7 @@ describe('Postman Request Authorization', function () {
     });
 
     it('should add api headers to request when api value is not given', function () {
-      var request = authRequests.API_KEY.KEY_HEADER;
+      let request = authRequests.API_KEY.KEY_HEADER;
       request = authorize(request);
       expect(request).to.be.of.instanceOf(sdk.Request);
       expect(request.headers.has('key')).to.be.true;
@@ -24,13 +24,13 @@ describe('Postman Request Authorization', function () {
     });
 
     it('should not add api headers to request when api key and value is not given', function () {
-      var request = authRequests.API_KEY.HEADER;
+      let request = authRequests.API_KEY.HEADER;
       request = authorize(request);
       expect(request).to.be.of.instanceOf(sdk.Request);
     });
 
     it('should add api key query param when api key value and in type is query', function () {
-      var request = authRequests.API_KEY.KEY_VAL_QUERY;
+      let request = authRequests.API_KEY.KEY_VAL_QUERY;
       request = authorize(request);
       expect(request).to.be.of.instanceOf(sdk.Request);
       expect(request.url.query.has('key')).to.be.true;
@@ -38,7 +38,7 @@ describe('Postman Request Authorization', function () {
     });
 
     it('should add api key query param when api key and in type is query', function () {
-      var request = authRequests.API_KEY.KEY_QUERY;
+      let request = authRequests.API_KEY.KEY_QUERY;
       request = authorize(request);
       expect(request).to.be.of.instanceOf(sdk.Request);
       expect(request.url.query.has('key')).to.be.true;
@@ -46,9 +46,54 @@ describe('Postman Request Authorization', function () {
     });
 
     it('should add api key query param when api key value and in type is query', function () {
-      var request = authRequests.API_KEY.QUERY;
+      let request = authRequests.API_KEY.QUERY;
       request = authorize(request);
       expect(request).to.be.of.instanceOf(sdk.Request);
+    });
+  });
+
+  describe('Bearer Token Authorization', function () {
+
+    it('should add authorization header to request when bearer token it given', function () {
+      let request = authRequests.BEARER_TOKEN.TOKEN;
+      request = authorize(request);
+      expect(request).to.be.instanceOf(sdk.Request);
+      expect(request.headers.has('Authorization')).to.be.true;
+      expect(request.getHeaders('Authorization').Authorization).to.be.equal('Bearer testbearertoken');
+    });
+  });
+
+  describe('Basic Authorization', function () {
+    it('should add authorization header when basic authorization is used with username and password', function () {
+      var request = authRequests.BASIC.USERNAME_PASSWORD;
+      request = authorize(request);
+      expect(request).to.be.instanceOf(sdk.Request);
+      expect(request.headers.has('Authorization')).to.be.true;
+      expect(request.getHeaders('Authorization').Authorization).to.be.equal('Basic dGVzdDp0ZXN0');
+    });
+
+    it('should add authorization header when basic authorization is used with username and no password', function () {
+      var request = authRequests.BASIC.USERNAME;
+      request = authorize(request);
+      expect(request).to.be.instanceOf(sdk.Request);
+      expect(request.headers.has('Authorization')).to.be.true;
+      expect(request.getHeaders('Authorization').Authorization).to.be.equal('Basic dGVzdDo=');
+    });
+
+    it('should add authorization header when basic authorization is used with just password', function () {
+      var request = authRequests.BASIC.PASSWORD;
+      request = authorize(request);
+      expect(request).to.be.instanceOf(sdk.Request);
+      expect(request.headers.has('Authorization')).to.be.true;
+      expect(request.getHeaders('Authorization').Authorization).to.be.equal('Basic OnRlc3Q=');
+    });
+
+    it('should add authorization header when basic authorization is used with no username/password', function () {
+      var request = authRequests.BASIC.NO_USERNAME_NO_PASS;
+      request = authorize(request);
+      expect(request).to.be.instanceOf(sdk.Request);
+      expect(request.headers.has('Authorization')).to.be.true;
+      expect(request.getHeaders('Authorization').Authorization).to.be.equal('Basic Og==');
     });
   });
 });
