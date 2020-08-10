772eada3dba2b6e908aa4947326c213f7c2204cb
Somesh Koli <kolisomesh27@gmail.com>
2020-08-11 05:11:45 +0530

    Adds basic unit tests   - adds basic unit test for generate method   - adds basic unit test for nodejs-sdkgen     - snippet test     - utils test

---
+++ sdkgen/nodejs-request/test/unit/snippet.test.js
@@ -0,0 +1,103 @@
+const expect = require('chai').expect,
+  sdk = require('postman-collection'),
+  utils = require('../../lib/util'),
+  fixtures = {
+    utilMethodOutputs: require('../fixtures/utilMethodOutputs.json')
+  },
+  collection = {
+    SDKGEN: require('../../../../test/fixtures/SDKGEN.postman_collection.json')
+  },
+  variables = {
+    SDKGEN: require('../../../../test/fixtures/SDKGEN_ENVIRONMENT.json')
+  },
+  generate = require('../../index').generate,
+  COLLECTION_INSTANCE = new sdk.Collection(collection.SDKGEN);
+
+describe('Tests for generated sdk', () => {
+
+  it('should generate sdk snippet', () => {
+    generate(COLLECTION_INSTANCE, {}, (err, snippet) => {
+      if (err) {
+        expect.fail(null, null, err);
+        return;
+      }
+      expect(snippet).to.be.a('string');
+    });
+  });
+
+  it('should include nodejs-request library import', () => {
+    generate(COLLECTION_INSTANCE, {}, (err, snippet) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.include('var request = require(\'request\')');
+    });
+  });
+
+
+  it('should add variable if provided', () => {
+    generate(COLLECTION_INSTANCE, {
+      variableList: new sdk.VariableList(null, variables.SDKGEN.values)
+    }, (err, snippet) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.include('\'url\': \'www.google.com\'');
+      expect(snippet).to.include('\'var1\': \'valenv1\'');
+      expect(snippet).to.include('\'var2\': \'valenv2\'');
+    });
+  });
+
+  it('should have default constructor for generated module', () => {
+    generate(COLLECTION_INSTANCE, {}, (err, snippet) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.include('function SDK(config = {})');
+    });
+  });
+
+  it('should have get/set Variable methods', () => {
+    generate(COLLECTION_INSTANCE, {}, (err, snippet) => {
+      if (err) {
+        expect.fail(null, null, err);
+        return;
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.include('SDK.prototype.setVariables = function (vars) {');
+      expect(snippet).to.include('SDK.prototype.getVariables = function (variable) {');
+    });
+  });
+
+  it('should set initial variables values', () => {
+    generate(COLLECTION_INSTANCE, {}, (err, snippet) => {
+      if (err) {
+        expect.fail(null, null, err);
+        return;
+      }
+      expect(snippet).to.be.a('string');
+      expect(snippet).to.include('this.variables = this.setVariables(config)')
+    });
+  });
+
+  it('should have self variable declaration', () => {
+    generate(COLLECTION_INSTANCE, {}, (err, snippet) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      expect(snippet).to.include('self = this');
+    });
+  });
+
+  it('should have export module snipept', () => {
+    generate(COLLECTION_INSTANCE, {}, (err, snippet) => {
+      if (err) {
+        expect.fail(null, null, err);
+      }
+      expect(snippet).to.include('module.exports = SDK');
+    });
+  });
+});
---
+++ sdkgen/nodejs-request/test/unit/util.test.js
@@ -0,0 +1,60 @@
+const expect = require('chai').expect,
+  sdk = require('postman-collection'),
+  utils = require('../../lib/util'),
+  outputs = require('../fixtures/utilMethodOutputs.json'),
+  collection = {
+    SDKGEN: require('../../../../test/fixtures/SDKGEN.postman_collection.json')
+  },
+  COLLECTION_INSTANCE = new sdk.Collection(collection.SDKGEN);
+
+describe('Util methods tests:', () => {
+  it('should sanitize strings properly', () => {
+    // eslint-disable-next-line quotes
+    let s = "test'string\\n\\";
+    s = utils.sanitize(s);
+    expect(s).to.be.a('string');
+    expect(s).to.equals(outputs.SANITIZE);
+  });
+
+  it('should generate function snippet for a request', async () => {
+    let snippet;
+    try {
+      snippet = await utils.generateFunctionSnippet(COLLECTION_INSTANCE.items.members[2], {});
+    }
+    catch (err) {
+      expect.fail(null, null, err);
+    }
+    expect(snippet).to.be.a('string');
+    expect(snippet).to.be.equals(outputs.GENERATE_FUNCTION_SNIPPEPT);
+  });
+
+  it('should generate item snippet for input item (parent --> collection instance)', async () => {
+    let snippet;
+    try {
+      snippet = await utils.itemHandler(COLLECTION_INSTANCE.items.members[2], {});
+    }
+    catch (err) {
+      expect.fail(null, null, err);
+    }
+    expect(snippet).to.be.a('string');
+    expect(snippet).to.be.equals(outputs.ITEM_HANDLER.COLLECTION_AS_PARENT);
+  });
+
+  it('should generate item snippet for input item (parent --> itemgroup instance)', async () => {
+    let snippet;
+    try {
+      snippet = await utils.itemHandler(COLLECTION_INSTANCE.items.members[0].items.members[0], {});
+    }
+    catch (err) {
+      expect.fail(null, null, err);
+    }
+    expect(snippet).to.be.a('string');
+    expect(snippet).to.include(outputs.ITEM_HANDLER.ITEMGROUP_AS_PARENT);
+  });
+
+  it('should generate snippet for get/set methods', () => {
+    let snippet = utils.getVariableFunctions();
+    expect(snippet).to.be.a('string');
+    expect(snippet).to.include(outputs.GET_VARIABLE_FUNCTIONS);
+  });
+});
