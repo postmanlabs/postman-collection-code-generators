3e5da4f0c6d126ab7ce2631c1f40ce1135a9df2b
Somesh Koli <kolisomesh27@gmail.com>
2020-08-24 00:27:25 +0530

    Added deep insatll and package script  - installs required scripts for a sdkgen  - generates list of available sdkgens

---
+++ npm/package.js
@@ -0,0 +1,41 @@
+const fs = require('fs'),
+  path = require('path'),
+  format = require('js-beautify'),
+  PATH_TO_ASSETS = path.resolve(__dirname, '../lib/assets');
+
+let sdkgenList = [],
+  sdkgenListSnippet,
+  formatedString;
+
+try {
+  sdkgenList = fs.readdirSync(path.join(__dirname, '..', 'sdkgen')).map((sdkgen) => {
+    let package = require(path.join(__dirname, '..', 'sdkgen', sdkgen, 'package.json')),
+      sdkgenDetails = {};
+    sdkgenDetails.name = package.name;
+    sdkgenDetails.language = package.language;
+    sdkgenDetails.variant = package.variant;
+    sdkgenDetails.extension = package.extension;
+    sdkgenDetails.path = `../../sdkgen/${package.name}`;
+    sdkgenDetails.module = `<<<require('../../sdkgen/${package.name}')>>>`;
+    return sdkgenDetails;
+  });
+}
+catch (err) {
+  console.log('Could not package all the SDK generators:');
+  console.log(err);
+}
+
+try {
+  sdkgenListSnippet = `let sdkgens = ${JSON.stringify(sdkgenList)
+    .replace(/"<<</, '')
+    .replace(/>>>"/, '')
+    .replace(/"/g, '\'')
+  };\nmodule.exports = sdkgens;\n`;
+  formatedString = format(sdkgenListSnippet, { indent_size: 2, space_in_empty_paren: true });
+  fs.writeFileSync(path.join(PATH_TO_ASSETS, 'sdkgens.js'), formatedString);
+}
+catch (error) {
+  console.log(error);
+}
+
+
